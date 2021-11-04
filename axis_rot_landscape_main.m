c1e0de92dbfcf73ea0f73a712fe07e81a3422364
Jiajie zhang <zhangjiajie043@gmail.com>
2021-11-04 09:54:33 +0800

    add quiver view

--- axis_rot_landscape_main.m
+++ axis_rot_landscape_main.m
@@ -23,8 +23,9 @@ crystal_n = [n; 1];
 sun_altitude = 20;
 sun_longitude = 0;
 ray_in = ll2xyz_with_gradient([sun_longitude, sun_altitude]);
-ray_out = zeros(total_num, 3);
-axis_rot = nan(size(ray_out));
+ray_out_ll = zeros(total_num, 2);
+jacob_store = zeros(2, 3, total_num);
+axis_rot = nan(total_num, 3);
 
 progress_cnt = 0;
 progress_bin = 0.005;
@@ -39,23 +40,40 @@ for i = 1:total_num
     curr_rot = [axis_u_ll(ll_idx, :), roll(roll_idx)];
     axis_rot(i, :) = curr_rot;
     
-    tmp_ll = crystal_system_with_gradient(curr_rot, [sun_longitude, sun_altitude], ...
+    [tmp_ll, tmp_jcb] = crystal_system_with_gradient(curr_rot, [sun_longitude, sun_altitude], ...
         crystal_face_norm, crystal_n);
-    ray_out(i, :) = ll2xyz_with_gradient(tmp_ll);
+    ray_out_ll(i, :) = tmp_ll;
+    jacob_store(:, :, i) = tmp_jcb;
 end
+ray_out = ll2xyz_with_gradient(ray_out_ll);
 bending_angle = acosd(ray_out * ray_in');
-ray_out_ll = xyz2ll_with_gradient(ray_out);
 
 %%
 xx = kron(axis_u_ll(:, 1), ones(size(roll)));
 yy = kron(axis_u_ll(:, 2), ones(size(roll)));
 zz = repmat(roll, [u_num, 1]);
+
+target_ll = [0, -5];
+dd_ll = bsxfun(@minus, ray_out_ll, target_ll);
 valid_idx = bending_angle > 0;
-valid_idx = valid_idx & abs(ray_out_ll(:, 1) - 0) < dr & abs(ray_out_ll(:, 2) - -5) < dr;
+valid_idx = valid_idx & max(abs(dd_ll), [], 2) < dr;
+
+uu = zeros(total_num, 1);
+vv = zeros(total_num, 1);
+ww = zeros(total_num, 1);
+for i = 1:total_num
+    tmp_uvw = dd_ll(i, :) * jacob_store(:, :, i);
+    uu(i) = tmp_uvw(1);
+    vv(i) = tmp_uvw(2);
+    ww(i) = tmp_uvw(3);
+end
 
 figure(1); clf;
+hold on;
 scatter3(zz(valid_idx), xx(valid_idx), yy(valid_idx), 20, ...
     bending_angle(valid_idx), 'fill');
+quiver3(zz(valid_idx), xx(valid_idx), yy(valid_idx), ...
+    ww(valid_idx), uu(valid_idx), vv(valid_idx));
 axis equal;
 set(gca, 'xlim', [0, 360], 'ylim', [-180, 180], 'zlim', [-90, 90]);
 colorbar;
