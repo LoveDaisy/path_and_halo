13d9ba97f89257bf0eb7dd2b990aac3e93a4f008
Jiajie zhang <zhangjiajie043@gmail.com>
2021-11-08 19:21:32 +0800

    update

--- contour_method_main.m
+++ contour_method_main.m
@@ -35,17 +35,17 @@ ray_in_xyz = ll2xyz_with_gradient(sun_ll);
 config = init_config_3d(crystal_face_norm, crystal_n, sun_ll, 3);
 
 %%
-halo_img_x = -4:.01:0;
-halo_img_y = 41.5:.01:42.5;
+halo_img_x = -4:.2:0;
+halo_img_y = -25:.2:0;
 halo_img = zeros(length(halo_img_y), length(halo_img_x));
 checked_pix = 0;
 progress_cnt = 0;
 progress_bin = 0.001;
 update_progress = false;
-for w = 53:length(halo_img_x)
+for w = 1:length(halo_img_x)
     for h = 1:length(halo_img_y)
-% for w = 315
-%     for h = 36
+% for w = 12
+%     for h = 18
         lon = halo_img_x(w);
         lat = halo_img_y(h);
 
@@ -86,7 +86,7 @@ for w = 53:length(halo_img_x)
         end
         halo_img(h, w) = weight;
         
-        if weight > 1e-8 && update_progress
+        if weight > 1e-6 && update_progress
             figure(1); clf;
             f1_pos = get(gcf, 'position');
             imagesc(halo_img_x, halo_img_y, log10(halo_img * 1e-2 ./ (halo_img + 1e-2) + 5e-5));
@@ -95,19 +95,40 @@ for w = 53:length(halo_img_x)
             drawnow;
             
             figure(2); clf;
-            set(gcf, 'position', f1_pos + [f1_pos(3), 0, 0, 0]);
-            subplot(2,1,1);
+            set(gcf, 'position', f1_pos + [f1_pos(3), 0, f1_pos(3), 0]);
+            subplot(2,2,1);
             hold on;
             for k = 1:length(x_contour)
                 plot(x_contour{k}(:, 2), x_contour{k}(:, 3), '-o');
                 plot(interp_rot_store{k}(:, 2), interp_rot_store{k}(:, 3), '.-');
                 title(sprintf('(%d,%d)\nw: %.4e', w, h, weight));
             end
-            subplot(2,1,2);
+            box on;
+            lat_lim = get(gca, 'xlim'); roll_lim1 = get(gca, 'ylim');
+            subplot(2,2,2);
+            hold on;
+            for k = 1:length(x_contour)
+                plot(x_contour{k}(:, 1), x_contour{k}(:, 3), '-o');
+                plot(interp_rot_store{k}(:, 1), interp_rot_store{k}(:, 3), '.-');
+                title(sprintf('(%d,%d)\nw: %.4e', w, h, weight));
+            end
+            box on;
+            lon_lim = get(gca, 'xlim'); roll_lim2 = get(gca, 'ylim');
+            subplot(2,2,1);
+            set(gca, 'ylim', [min(roll_lim1(1), roll_lim2(1)), max(roll_lim1(2), roll_lim2(2))]);
+            subplot(2,2,2);
+            set(gca, 'ylim', [min(roll_lim1(1), roll_lim2(1)), max(roll_lim1(2), roll_lim2(2))]);
+            subplot(2,2,[3,4]);
             hold on;
             for k = 1:length(interp_p_store)
-                plot(interp_p_store{k}(:, 1), interp_p_store{k}(:, 2), '.-');
+                plot(interp_p_store{k}(:, 1), interp_p_store{k}(:, 2), '.-k', 'linewidth', 2);
+                plot(interp_p_store{k}(:, 1), interp_p_store{k}(:, 3), '.-');
+                plot(interp_p_store{k}(:, 1), interp_p_store{k}(:, 4), '--', 'linewidth', 2);
+                plot(interp_p_store{k}(:, 1), interp_p_store{k}(:, 5), ':', 'linewidth', 3);
             end
+            plot([interp_p_store{1}(1,1), interp_p_store{1}(end,1)], [1, 1], ':k');
+            set(gca, 'yscale', 'log', 'ylim', [1e-8, 1e4]);
+            box on;
             drawnow;
         end
     end
