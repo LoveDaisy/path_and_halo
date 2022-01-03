27bf65850431c19b8fcbef7a5aed14a1bc07991b
Jiajie zhang <zhangjiajie043@gmail.com>
2022-01-03 11:15:38 +0800

    update llr2quat

--- +geo/llr2quat.m
+++ +geo/llr2quat.m
@@ -1,30 +1,52 @@
-function [q, grad_q] = llr2quat(llr)
+function [q, jac] = llr2quat(llr)
+% Convert a 3D rotation from [longitude, latitude, roll] to quaternion [w, x, y, z]
+% A rotation can be represented by an axis direction and a roll around the axis.
+% Specially in this project, we use a direction of crystal main axis and a roll around it, which
+% is a little different from nomal axis-angle representation
+% (see https://en.wikipedia.org/wiki/Axis%E2%80%93angle_representation)
+% The rotation matrix is (for column vector, rotation matrix is left-multiplied):
+%   rotz(90 + longitude) * rotx(90 - latitude) * rotz(roll)
+% which means:
+%   1. apply roll around z-axis to object,
+%   2. apply 90 - latitude around x-axis to object,
+%   3. apply 90 + longitude around z-axis to object.
+%
 % INPUT
+%   llr:        n*3, [longitude, latitude, roll], in degree
+%
+% OUTPUT
+%   q:          n*4, quaternion. Apply quaternion using quatrotate(q, v) to row vector has the same
+%               result with using matrix like v * mat' (v is row vector thus the matrix is transposed
+%               and right-multiplied)
+%   jac :       4*3*n, Jacobian of this transformation
 
-% The rotation matrix is:
-%  rotz(90 + llr(1)) * rotx(90 - llr(2)) * rotz(llr(3))
+p = inputParser;
+p.addRequired('llr', @(x) validateattributes(x, {'numeric'}, {'2d', 'ncols', 3}));
+p.parse(llr);
+
+% Construct three quaternion corresponding to three rotation matrix
 q1 = [cosd(llr(:, 3) / 2), -sind(llr(:, 3) / 2) * [0, 0, 1]];
 q2 = [cosd(45 - llr(:, 2) / 2), -sind(45 - llr(:, 2) / 2) * [1, 0, 0]];
 q3 = [cosd(45 + llr(:, 1) / 2), -sind(45 + llr(:, 1) / 2) * [0, 0, 1]];
 qm = quatmultiply(q1, q2);
 q = quatmultiply(qm, q3);
-[q, grad_norm] = geo.normalize_vector(q);
+[q, jac_norm] = geo.normalize_vector(q);
 
 num = size(llr, 1);
-grad_q = nan(4, 3, num);
+jac = nan(4, 3, num);
 for i = 1:num
     j1 = [0, 0, -sind(llr(i, 3) / 2);
         0, 0, 0;
         0, 0, 0;
-        0, 0, -cosd(llr(i, 3) / 2)] * pi / 180 / 2;
+        0, 0, -cosd(llr(i, 3) / 2)] * pi / 180/2;
     j2 = [0, sind(45 - llr(i, 2) / 2), 0;
         0, cosd(45 - llr(i, 2) / 2), 0;
         0, 0, 0;
-        0, 0, 0] * pi / 180 / 2;
+        0, 0, 0] * pi / 180/2;
     j3 = [-sind(45 + llr(i, 1) / 2), 0, 0;
         0, 0, 0;
         0, 0, 0;
-        -cosd(45 + llr(i, 1) / 2), 0, 0] * pi / 180 / 2;
+        -cosd(45 + llr(i, 1) / 2), 0, 0] * pi / 180/2;
     jm = [q2(i, 1), -q2(i, 2), -q2(i, 3), -q2(i, 4);
         q2(i, 2), q2(i, 1), q2(i, 4), -q2(i, 3);
         q2(i, 3), -q2(i, 4), q2(i, 1), q2(i, 2);
@@ -33,7 +55,7 @@ for i = 1:num
         q1(i, 2), q1(i, 1), -q1(i, 4), q1(i, 3);
         q1(i, 3), q1(i, 4), q1(i, 1), -q1(i, 2);
         q1(i, 4), -q1(i, 3), q1(i, 2), q1(i, 1)] * j2;
-    jacob = [q3(i, 1), -q3(i, 2), -q3(i, 3), -q3(i, 4);
+    jq = [q3(i, 1), -q3(i, 2), -q3(i, 3), -q3(i, 4);
         q3(i, 2), q3(i, 1), q3(i, 4), -q3(i, 3);
         q3(i, 3), -q3(i, 4), q3(i, 1), q3(i, 2);
         q3(i, 4), q3(i, 3), -q3(i, 2), q3(i, 1)] * jm + ...
@@ -41,6 +63,6 @@ for i = 1:num
         qm(i, 2), qm(i, 1), -qm(i, 4), qm(i, 3);
         qm(i, 3), qm(i, 4), qm(i, 1), -qm(i, 2);
         qm(i, 4), -qm(i, 3), qm(i, 2), qm(i, 1)] * j3;
-    grad_q(:, :, i) = grad_norm(:, :, i) * jacob;
+    jac(:, :, i) = jac_norm(:, :, i) * jq;
 end
 end
