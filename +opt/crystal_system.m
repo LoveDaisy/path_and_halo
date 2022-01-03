5d39700c4194b4bf848e10b21ed4cd1bd69d7dc2
Jiajie zhang <zhangjiajie043@gmail.com>
2022-01-03 16:28:28 +0800

    update & format for crystal_system

--- +opt/crystal_system.m
+++ +opt/crystal_system.m
@@ -1,12 +1,16 @@
-function [ray_out_ll, g_rot] = crystal_system(rot_llr, ray_in_ll, crystal, trace)
+function [ray_out_ll, jac] = crystal_system(rot_llr, ray_in_ll, crystal, trace)
+% Find output ray of a crystal system with given input ray and crystal rotation.
+%
 % INPUT
-%   rot_llr:            n*3
-%   ray_in_ll:          1*2
+%   rot_llr:            n*3, [longitude, latitude, roll], in degree. A representation of crystal rotation.
+%                       see help of geo.llr2quat(llr) for detail.
+%   ray_in_ll:          1*2, [longitude, latitude], in degree. Input ray direction.
 %   crystal:            struct
-%       .face_norm      m*3
 %   trace:              struct
-%       .fid            k*1
-%       .n:             k*1
+%
+% OUTPUT
+%   ray_out_ll:         n*2, [longitude, latitude], in degree. Output ray direction.
+%   jac:                2*3*n, Jacobian. Input is rotation llr and output is ray_out_ll
 
 num = size(rot_llr, 1);
 
@@ -24,16 +28,16 @@ sq3 = sind(q3);
 g_r_mat = zeros(3, 3, num, 3);
 g_r_mat(:, :, :, 1) = reshape( ...
     [-cq1 .* cq3 + sq1 .* sq2 .* sq3, -sq1 .* cq3 - cq1 .* sq2 .* sq3, zeros(num, 1), ...
-    sq1 .* sq2 .* cq3 + cq1 .* sq3, -cq1 .* sq2 .* cq3 + sq1 .* sq3, zeros(num, 1), ...
-    -sq1 .* cq2, cq1 .* cq2, zeros(num, 1)]', [3, 3, num, 1]) * pi / 180;
+        sq1 .* sq2 .* cq3 + cq1 .* sq3, -cq1 .* sq2 .* cq3 + sq1 .* sq3, zeros(num, 1), ...
+        -sq1 .* cq2, cq1 .* cq2, zeros(num, 1)]', [3, 3, num, 1]) * pi / 180;
 g_r_mat(:, :, :, 2) = reshape( ...
     [-cq1 .* cq2 .* sq3, -sq1 .* cq2 .* sq3, -sq2 .* sq3, ...
-    -cq1 .* cq2 .* cq3, -sq1 .* cq2 .* cq3, -sq2 .* cq3, ...
-    -cq1 .* sq2, -sq1 .* sq2, cq2]', [3, 3, num, 1]) * pi / 180;
+        -cq1 .* cq2 .* cq3, -sq1 .* cq2 .* cq3, -sq2 .* cq3, ...
+        -cq1 .* sq2, -sq1 .* sq2, cq2]', [3, 3, num, 1]) * pi / 180;
 g_r_mat(:, :, :, 3) = reshape( ...
     [-cq1 .* sq2 .* cq3 + sq1 .* sq3, -sq1 .* sq2 .* cq3 - cq1 .* sq3, cq2 .* cq3, ...
-    sq1 .* cq3 + cq1 .* sq2 .* sq3, -cq1 .* cq3 + sq1 .* sq2 .* sq3, -cq2 .* sq3, ...
-    zeros(num, 3)]', [3, 3, num, 1]) * pi / 180;
+        sq1 .* cq3 + cq1 .* sq2 .* sq3, -cq1 .* cq3 + sq1 .* sq2 .* sq3, -cq2 .* sq3, ...
+        zeros(num, 3)]', [3, 3, num, 1]) * pi / 180;
 
 ray_in_xyz = zeros(num, 3);
 rot_mat_store = zeros(3, 3, num);
@@ -49,13 +53,13 @@ g_xyz = zeros(3, 3, num);
 for i = 1:num
     g_xyz1 = [xyz0 * g_r_mat(:, :, i, 1); xyz0 * g_r_mat(:, :, i, 2); xyz0 * g_r_mat(:, :, i, 3)]';
     g_xyz(:, :, i) = [ray_out_xyz(i, :) * g_r_mat(:, :, i, 1)'; ray_out_xyz(i, :) * g_r_mat(:, :, i, 2)'; ...
-        ray_out_xyz(i, :) * g_r_mat(:, :, i, 3)']' +  rot_mat_store(:, :, i) * g_out(:, :, i) * g_xyz1;
+                    ray_out_xyz(i, :) * g_r_mat(:, :, i, 3)']' + rot_mat_store(:, :, i) * g_out(:, :, i) * g_xyz1;
     ray_out_xyz(i, :) = ray_out_xyz(i, :) * rot_mat_store(:, :, i)';
 end
 
 [ray_out_ll, g_ll] = geo.xyz2ll(ray_out_xyz);
-g_rot = zeros(2, 3, num);
+jac = zeros(2, 3, num);
 for i = 1:num
-    g_rot(:, :, i) = g_ll(:, :, i) * g_xyz(:, :, i);
+    jac(:, :, i) = g_ll(:, :, i) * g_xyz(:, :, i);
 end
 end
