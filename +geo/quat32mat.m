7a8d08b0e28fddd6c999b35e5dd9756e1177963e
Jiajie zhang <zhangjiajie043@gmail.com>
2022-01-04 18:49:53 +0800

    bugfix for quat32mat
    for those norm(quat3) > 1

--- +geo/quat32mat.m
+++ +geo/quat32mat.m
@@ -17,8 +17,20 @@ p.parse(quat3);
 num = size(quat3, 1);
 
 n2 = sum(quat3.^2, 2);
+invalid_idx = n2 >= 1;
 
-w = sqrt(max(1 - n2, 1e-10));
+jac_norm = nan(3, 3, num);
+if any(invalid_idx)
+    [quat3(invalid_idx, :), tmp_jac_norm] = geo.normalize_vector(quat3(invalid_idx, :));
+    jac_norm(:, :, invalid_idx) = tmp_jac_norm;
+end
+for i = 1:num
+    if ~invalid_idx(i)
+        jac_norm(:, :, i) = eye(3);
+    end
+end
+
+w = sqrt(max(1 - n2, 0));
 x = quat3(:, 1);
 y = quat3(:, 2);
 z = quat3(:, 3);
@@ -28,20 +40,39 @@ z = quat3(:, 3);
 %   2*x*y - 2*z*w,      1 - 2*x^2 - 2*z^2,  2*y*z + 2*x*w
 %   2*x*z + 2*y*w,      2*y*z - 2*x*w,      1 - 2*x^2 - 2*y^2
 
+w1 = -x ./ w;
+w1(invalid_idx) = 0;
+w2 = -y ./ w;
+w2(invalid_idx) = 0;
+w3 = -z ./ w;
+w3(invalid_idx) = 0;
+
 mat = nan(3, 3, num);
 jac = nan(3, 3, 3, num);
 for i = 1:num
-    mat(:, :, i) = [1 - 2 * y(i)^2 - 2 * z(i)^2, 2 * x(i) * y(i) + 2 * z(i) * w(i), 2 * x(i) * z(i) - 2 * y(i) * w(i);
-                2 * x(i) * y(i) - 2 * z(i) * w(i), 1 - 2 * x(i)^2 - 2 * z(i)^2, 2 * y(i) * z(i) + 2 * x(i) * w(i);
-                2 * x(i) * z(i) + 2 * y(i) * w(i), 2 * y(i) * z(i) - 2 * x(i) * w(i), 1 - 2 * x(i)^2 - 2 * y(i)^2];
-    jac(:, :, 1, i) = [0, 2 * y(i) - 2 * z(i) * x(i) / w(i), 2 * z(i) + 2 * y(i) * x(i) / w(i);
-                    2 * y(i) + 2 * z(i) * x(i) / w(i), -4 * x(i), 2 * w(i) - 2 * x(i)^2 / w(i);
-                    2 * z(i) - 2 * y(i) * x(i) / w(i), -2 * w(i) + 2 * x(i)^2 / w(i), -4 * x(i)];
-    jac(:, :, 2, i) = [-4 * y(i), 2 * x(i) - 2 * z(i) * y(i) / w(i), -2 * w(i) + 2 * y(i)^2 / w(i);
-                    2 * x(i) + 2 * z(i) * y(i) / w(i), 0, 2 * z(i) - 2 * x(i) * y(i) / w(i);
-                    2 * w(i) - 2 * y(i)^2 / w(i), 2 * z(i) + 2 * x(i) * y(i) / w(i), -4 * y(i)];
-    jac(:, :, 3, i) = [-4 * z(i), 2 * w(i) - 2 * z(i)^2 / w(i), 2 * x(i) + 2 * y(i) * z(i) / w(i);
-                    -2 * w(i) + 2 * z(i)^2 / w(i), -4 * z(i), 2 * y(i) - 2 * x(i) * z(i) / w(i);
-                    2 * x(i) - 2 * y(i) * z(i) / w(i), 2 * y(i) + 2 * x(i) * z(i) / w(i), 0];
+    m = [1 - 2 * y(i)^2 - 2 * z(i)^2, 2 * x(i) * y(i) + 2 * z(i) * w(i), 2 * x(i) * z(i) - 2 * y(i) * w(i);
+        2 * x(i) * y(i) - 2 * z(i) * w(i), 1 - 2 * x(i)^2 - 2 * z(i)^2, 2 * y(i) * z(i) + 2 * x(i) * w(i);
+        2 * x(i) * z(i) + 2 * y(i) * w(i), 2 * y(i) * z(i) - 2 * x(i) * w(i), 1 - 2 * x(i)^2 - 2 * y(i)^2];
+    jac1 = [0, 2 * y(i) + 2 * z(i) * w1(i), 2 * z(i) - 2 * y(i) * w1(i);
+        2 * y(i) - 2 * z(i) * w1(i), -4 * x(i), 2 * w(i) + 2 * x(i) * w1(i);
+        2 * z(i) + 2 * y(i) * w1(i), -2 * w(i) - 2 * x(i) * w1(i), -4 * x(i)];
+    jac2 = [-4 * y(i), 2 * x(i) + 2 * z(i) * w2(i), -2 * w(i) - 2 * y(i) * w2(i);
+        2 * x(i) - 2 * z(i) * w2(i), 0, 2 * z(i) + 2 * x(i) * w2(i);
+        2 * w(i) + 2 * y(i) * w2(i), 2 * z(i) - 2 * x(i) * w2(i), -4 * y(i)];
+    jac3 = [-4 * z(i), 2 * w(i) + 2 * z(i) * w3(i), 2 * x(i) - 2 * y(i) * w3(i);
+        -2 * w(i) - 2 * z(i) * w3(i), -4 * z(i), 2 * y(i) + 2 * x(i) * w3(i);
+        2 * x(i) + 2 * y(i) * w3(i), 2 * y(i) - 2 * x(i) * w3(i), 0];
+
+    mat(:, :, i) = m;
+    if invalid_idx(i)
+        tmp_jac = [jac1(:), jac2(:), jac3(:)] * jac_norm(:, :, i);
+        jac(:, :, 1, i) = reshape(tmp_jac(:, 1), [3, 3]);
+        jac(:, :, 2, i) = reshape(tmp_jac(:, 2), [3, 3]);
+        jac(:, :, 3, i) = reshape(tmp_jac(:, 3), [3, 3]);
+    else
+        jac(:, :, 1, i) = jac1;
+        jac(:, :, 2, i) = jac2;
+        jac(:, :, 3, i) = jac3;
+    end
 end
 end
--- +geo/test_quat32mat.m
+++ +geo/test_quat32mat.m
@@ -1,5 +1,5 @@
 function test_quat32mat()
-test_cases = {@suite1, @suite2};
+test_cases = {@suite1, @suite2, @suite3};
 num = length(test_cases);
 
 fprintf('Start testing for quat32mat...\n');
@@ -53,4 +53,23 @@ mat3 = geo.quat32mat(bsxfun(@plus, quat3, [0, 0, h]));
 jac0 = cat(4, (mat1 - mat) / h, (mat2 - mat) / h, (mat3 - mat) / h);
 jac0 = permute(jac0, [1, 2, 4, 3]);
 assert(all(abs(jac(:) - jac0(:)) < 8e-4));
+end
+
+% ================================================================================
+function suite3()
+quat3 = [2, -1, 5];
+quat3 = quat3 / norm(quat3);
+q = [sqrt(1 - norm(quat3)^2), quat3];
+
+mat0 = quatrotate(q, eye(3))';
+
+[mat, jac] = geo.quat32mat(quat3);
+assert(all(abs(mat(:) - mat0(:)) < 1e-8));
+
+h = 1e-6;
+mat1 = quatrotate([0, quat3 + [h, 0, 0]], eye(3))';
+mat2 = quatrotate([0, quat3 + [0, h, 0]], eye(3))';
+mat3 = quatrotate([0, quat3 + [0, 0, h]], eye(3))';
+jac0 = cat(3, (mat1 - mat) / h, (mat2 - mat) / h, (mat3 - mat) / h);
+assert(all(abs(jac(:) - jac0(:)) < 5e-4));
 end
