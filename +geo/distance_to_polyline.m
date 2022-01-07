326245e433b5cdcfe77d14bf996ec4c75808ffa4
Jiajie zhang <zhangjiajie043@gmail.com>
2022-01-07 21:27:22 +0800

    add reduce_pts_polyline

--- +geo/distance_to_polyline.m
+++ +geo/distance_to_polyline.m
@@ -1,4 +1,4 @@
-function [d, v, it] = distance_to_polyline(line_pts, p0)
+function [d, v, it, p] = distance_to_polyline(line_pts, p0)
 % Find the distance between given point(s) to a polyline
 %
 % INPUT
@@ -9,6 +9,7 @@ function [d, v, it] = distance_to_polyline(line_pts, p0)
 %   d:              m*1, distance
 %   v:              m*d, vector from query point to the nerest point on polyline
 %   it:             m*2, line segment index and normalized length parameter t
+%   v:              m*d, projected point on lines
 
 m = size(p0, 1);
 dim = size(p0, 2);
@@ -21,15 +22,17 @@ p.parse(line_pts, p0);
 d = nan(m, 1);
 v = nan(m, dim);
 it = nan(m, 2);
+p = nan(m, dim);
 for i = 1:m
-    [tmp_dist, tmp_vn, tmp_t] = p2line_dist(p0(i, :), line_pts(1:end - 1, :), line_pts(2:end, :));
+    [tmp_dist, tmp_vn, tmp_t, tmp_p0] = p2line_dist(p0(i, :), line_pts(1:end - 1, :), line_pts(2:end, :));
     [d(i), tmp_idx] = min(tmp_dist);
     v(i, :) = tmp_vn(tmp_idx, :);
     it(i, :) = [tmp_idx, tmp_t(tmp_idx)];
+    p(i, :) = tmp_p0(tmp_idx, :);
 end
 end
 
-function [d, v, t] = p2line_dist(p, xy1, xy2)
+function [d, v, t, p0] = p2line_dist(p, xy1, xy2)
 % Helper function. Find the distance between given point to several line segments
 %
 % INPUT
@@ -40,12 +43,13 @@ function [d, v, t] = p2line_dist(p, xy1, xy2)
 %   d:          scalar, distance
 %   v:          n*d, vector from query point to every line segment
 %   t:          n*1, normalized length parameter
+%   p0:         n*d, projected point on lines
 
-vp = bsxfun(@minus, p, xy1);
+v1 = bsxfun(@minus, p, xy1);
 v2 = xy2 - xy1;
 
-t = min(max(sum(vp .* v2, 2) ./ sum(v2.^2, 2), 0), 1);
-vp_proj = bsxfun(@times, t, v2);
-v = bsxfun(@minus, vp_proj, vp);
+t = min(max(sum(v1 .* v2, 2) ./ sum(v2.^2, 2), 0), 1);
+p0 = bsxfun(@times, t, v2);
+v = bsxfun(@minus, p0, v1);
 d = sqrt(sum(v.^2, 2));
 end
---
+++ +geo/reduce_pts_polyline.m
@@ -0,0 +1,57 @@
+function [pts, status] = reduce_pts_polyline(polyline, pts, varargin)
+% Remove points that locate very close to given polyline.
+% Result pts is normal formed. For llr representation, it means all longitudes will be in
+% [-180, 180] (the same as atan2), all latitudes will be in [-90, 90] and rolls will be in [0, 360]
+%
+% INPUT
+%   polyline:       n*d
+%   pts:            m*d, all points to be checked.
+%
+% OPTION
+%   'jac_fun':      function handle, Jacobian function, computes the Jacobian to a target space,
+%                   in which distance can be evaluated. It must be in form of jac = fun(x) for
+%                   x is n*d and jac is d1*d*n, where d is dimesion of input space and d1 is
+%                   dimesion of output space.
+%   'eps':          tolarence to decide whether a point is close to polyline.
+%
+% OUTPUT
+%   pts:            m1*d, reduced points.
+%   status:         struct
+
+status.fun_eval_cnt = 0;
+if isempty(pts)
+    return;
+end
+
+p = inputParser;
+p.addParameter('eps', 1e-4, @(x) validateattributes(x, {'double'}, {'positive'}));
+p.addParameter('jac_fun', [], @(x) validateattributes(x, {'function_handle'}, {'scalar'}));
+p.parse(varargin{:});
+
+num = size(pts, 1);
+dim1 = size(pts, 2);
+if dim1 == 3
+    pts0 = pts;
+    pts0(:, 1) = mod(pts(:, 1), 360) - 180;
+    pts0(:, 2) = mod(pts(:, 2), 180) - 90;
+    pts0(:, 3) = mod(pts(:, 3), 360);
+
+    polyline(:, 1) = mod(polyline(:, 1), 360) - 180;
+    polyline(:, 2) = mod(polyline(:, 2), 180) - 90;
+    polyline(:, 3) = mod(polyline(:, 3), 360);
+else
+    pts0 = pts;
+end
+
+[d, v, ~, p0] = geo.distance_to_polyline(polyline, pts0);
+if ~isempty(p.Results.jac_fun)
+    jac = p.Results.jac_fun(p0);
+    status.fun_eval_cnt = status.fun_eval_cnt + 1;
+    for i = 1:num
+        d(i) = norm(jac * v(i, :)');
+    end
+end
+
+dup_idx = d < p.Results.eps;
+pts = pts(~dup_idx, :);
+end
