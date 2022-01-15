9b3397bcf6637867a72fac7ddc552bf5bda080ab
Jiajie zhang <zhangjiajie043@gmail.com>
2022-01-16 02:20:28 +0800

    add find_cand_rot()
    just find candidates rather than solve them

---
+++ +opt/find_cand_rot.m
@@ -0,0 +1,16 @@
+function cand_rot = find_cand_rot(config, target_ll, type)
+out_ll_diff = acosd(geo.ll2xyz(config.out_ll) * geo.ll2xyz(target_ll)');
+[out_ll_diff, sort_idx] = sort(out_ll_diff);
+
+cand_idx = sort_idx(out_ll_diff < config.dr);
+cand_llr = config.axis_llr_store(cand_idx, :);
+cand_quat = config.axis_quat_store(cand_idx, :);
+
+if strcmpi(type, 'llr')
+    cand_rot = cand_llr;
+elseif strcmpi(type, 'quat')
+    cand_rot = cand_quat;
+else
+    error('unknown rotation type!');
+end
+end
