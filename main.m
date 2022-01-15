25adc1c33ed23e7b1d4dd707d8a160d5c3ca9384
Jiajie zhang <zhangjiajie043@gmail.com>
2022-01-16 02:22:01 +0800

    update main

--- main.m
+++ main.m
@@ -5,8 +5,8 @@ crystal = opt.make_prism_crystal(1);
 trace.fid = [3; 5];
 
 axis_pdf = geo.make_axis_pdf([1, 90, 0.2], []);
+% axis_pdf = geo.make_axis_pdf([0, 0, 0], []);
 
-%%
 sun_ll = [180, 10];
 ray_in_ll = [sun_ll(1) + 180, -sun_ll(2)];
 ray_in_xyz = geo.ll2xyz(ray_in_ll);
@@ -21,17 +21,17 @@ close all;
 use_rot_quat = true;
 
 tic;
-halo_img = generate_halo_image([0, 10], [0, 30], 0.5);
+halo_img = generate_halo_image([0, 10], [-60, -20], 0.5);
 
 fun_eval_cnt = 0;
 checked_pix = 0;
 progress_cnt = 0;
-progress_bin = 0.005;
+progress_bin = 0.001;
 update_progress = false;
 for w = 1:halo_img.x_length
     for h = 1:halo_img.y_length
-% for w = 8
-%     for h = 36
+% for w = 16
+%     for h = 17
         ray_out_ll = [halo_img.img_x(w), halo_img.img_y(h)];
 
         checked_pix = checked_pix + 1;
@@ -48,41 +48,73 @@ for w = 1:halo_img.x_length
 
         % Find seed rotation
         if use_rot_quat
-            [seed_rot, seed_status] = opt.find_seed_rot(config, ray_out_ll, 'quat');
+            cand_rot = opt.find_cand_rot(config, ray_out_ll, 'quat');
             contour_h = 0.05;
             reduce_eps = 0.05;
         else
-            [seed_rot, seed_status] = opt.find_seed_rot(config, ray_out_ll, 'llr');
+            cand_rot = opt.find_cand_rot(config, ray_out_ll, 'llr');
             contour_h = 1;
             reduce_eps = 0.5;
         end
-        fun_eval_cnt = fun_eval_cnt + seed_status.fun_eval_cnt;
-        if isempty(seed_rot) || size(seed_rot, 1) > 100
+        if isempty(cand_rot)
             continue;
         end
+        
+        figure(5); clf;
+        plot_data_3d(cand_rot, [2,3,4], 'o');
+        hold on;
+        axis equal;
 
         weight = 0;
         cmp_store = {};
-        rot_store = {};
-        contour_store = {};
+        interp_llr_store = {};
+        llr_contour_store = {};
+        rot_contour_store = {};
         k = 1;
-        while ~isempty(seed_rot)
-            % Find contour
-            [rot_contour, contour_status] = ode.find_contour(fdf, seed_rot(1, :), 'h', contour_h);
-            seed_rot = seed_rot(2:end, :);
-            fun_eval_cnt = fun_eval_cnt + contour_status.fun_eval_cnt;
-            if isempty(rot_contour)
+        while ~isempty(cand_rot)
+            % Find initial solution
+            [init_rot, sol_status] = ode.find_solution(fdf, cand_rot(1, :), [ray_out_ll, 1], 'eps', 1e-8);
+            fun_eval_cnt = fun_eval_cnt + sol_status.fun_eval_cnt;
+            cand_rot = cand_rot(2:end, :);
+            if ~sol_status.finish
+                continue;
+            end
+            
+            % Check if it locates on previous contour
+            duplicated = false;
+            for i = 1:k-1
+                tmp_d = geo.distance_to_polyline(rot_contour_store{i}, init_rot);
+                if tmp_d < reduce_eps
+                    duplicated = true;
+                    break;
+                end
+            end
+            if duplicated
                 continue;
             end
 
+            % Find contour
+            [rot_contour, contour_status] = ode.find_contour(fdf, init_rot, 'h', contour_h);
+            fun_eval_cnt = fun_eval_cnt + contour_status.fun_eval_cnt;
+
+            if isempty(rot_contour)
+                continue;
+            end
+            
+            figure(5);
+            plot_data_3d(rot_contour, [2,3,4], '-x');
+            drawnow;
+
+            rot_contour_store{k} = rot_contour;
             if use_rot_quat
-                contour_store{k} = geo.quat2llr(rot_contour);
+                llr_contour_store{k} = geo.quat2llr(rot_contour);
             else
-                contour_store{k} = rot_contour;
+                llr_contour_store{k} = rot_contour;
             end
 
             % Reduce seeds
-            [seed_rot, reduce_status] = geo.reduce_pts_polyline(rot_contour, seed_rot, 'eps', reduce_eps);
+            [cand_rot, reduce_status] = geo.reduce_pts_polyline(rot_contour, cand_rot, ...
+                'eps', config.dr * 1, 'jac_fun', fdf);
             fun_eval_cnt = fun_eval_cnt + reduce_status.fun_eval_cnt;
             if size(rot_contour, 1) < 2
                 continue;
@@ -92,7 +124,7 @@ for w = 1:halo_img.x_length
             [curr_w, curr_cmp, curr_rot] = opt.compute_contour_weight(rot_contour, axis_pdf, config);
             weight = weight + curr_w;
             cmp_store{k} = curr_cmp;
-            rot_store{k} = curr_rot;
+            interp_llr_store{k} = curr_rot;
             k = k + 1;
         end
         halo_img.img(h, w) = weight;
@@ -100,25 +132,23 @@ for w = 1:halo_img.x_length
             continue;
         end
 
-        if update_progress && weight > 1e-8
+%         if update_progress
             figure(1); clf;
-            vis_range = [5e-4, 1];
             f1_pos = get(gcf, 'position');
-            show_halo_img(halo_img, vis_range);
+            show_halo_img(halo_img);
             title(sprintf('(%d,%d)\nw: %.4e', w, h, weight));
             drawnow;
 
             figure(2); clf;
             set(gcf, 'position', f1_pos + [f1_pos(3), 0, f1_pos(3), 0]);
-            show_contour_info(contour_store, rot_store, cmp_store);
+            show_contour_info(llr_contour_store, interp_llr_store, cmp_store);
             drawnow;
-        end
+%         end
     end
 end
 toc;
 
 %%
 figure(1); clf;
-vis_range = [5e-4, 1];
-show_halo_img(halo_img, vis_range);
+show_halo_img(halo_img);
 
