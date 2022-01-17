48c9cedef3792c39a45beae5f351dcd04d96c6d0
Jiajie zhang <zhangjiajie043@gmail.com>
2022-01-17 14:12:21 +0800

    minor update

--- show_halo_img.m
+++ show_halo_img.m
@@ -5,7 +5,7 @@ function show_halo_img(halo_img)
 %   halo_img:       struct, generated from generate_halo_img()
 
 vis_range = prctile(halo_img.img(:), [10, 90]);
-vis_range = max(vis_range, 1e-3 * vis_range(2));
+vis_range = max(vis_range, 3e-3 * vis_range(2));
 imagesc(halo_img.img_x, halo_img.img_y, halo_vis_fun(halo_img.img, vis_range));
 axis equal; axis tight; axis xy;
 end
