f42fd2821ab9812087a80af6e5aa3197ed9857d4
Jiajie zhang <zhangjiajie043@gmail.com>
2022-01-18 11:43:48 +0800

    add dynamic range for show_halo_img

--- show_halo_img.m
+++ show_halo_img.m
@@ -1,11 +1,16 @@
-function show_halo_img(halo_img)
+function show_halo_img(halo_img, dr)
 % Display halo image.
 %
 % INPUT
 %   halo_img:       struct, generated from generate_halo_img()
 
+if nargin == 1
+    dr = 6.5;
+end
+k = 2^(-dr);
+
 vis_range = prctile(halo_img.img(:), [10, 90]);
-vis_range = max(vis_range, 3e-3 * vis_range(2));
+vis_range = max(vis_range, k * vis_range(2));
 imagesc(halo_img.img_x, halo_img.img_y, halo_vis_fun(halo_img.img, vis_range));
 axis equal; axis tight; axis xy;
 end
