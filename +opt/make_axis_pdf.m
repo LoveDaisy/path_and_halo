5ca79be9341620988cd3d6b03518a445a36052f4
Jiajie zhang <zhangjiajie043@gmail.com>
2022-01-24 10:47:05 +0800

    update make_axis_pdf

--- +opt/make_axis_pdf.m
+++ +opt/make_axis_pdf.m
@@ -1,30 +1,42 @@
-function axis_pdf = make_axis_pdf(zenith_dist, roll_dist)
+function axis_pdf = make_axis_pdf(zenith, roll)
 % Make axis distribution PDF.
 %
 % INPUT
-%   zenith_dist:            [mean, sigma] or []
-%   roll_dist:              [mean, sigma] or []
+%   zenith:         [mean, sigma] or []
+%   roll:           [mean, sigma] or []
+%
+% OUTPUT
+%   axis_pdf:       function handle. It has the form: p = fun(llr).
+%                   It can be decomposed into two parts, [longitude, latitude] is direction part,
+%                   [roll] is rotation part. These two part can be integrated separately.
+%                       integrate(pdf_ll, d_lat, d_lon) * integrate(pdf_r, d_roll) = 1
+%                   So we can normalize them to 1 respectively, and take the total integration constant
+%                   at last.
 
-if nargin == 0 || isempty(zenith_dist)
+% First part, axis direction.
+% This PDF represents the density of axis direction, i.e. the probability per unit area on sphere.
+% It is normalized so that C * integrate(pdf_ll, d_area) = 1.
+if nargin == 0 || isempty(zenith)
     % Uniform distribution
-    a_pdf = @(llr) ones(size(llr, 1), 1) / (180 * 180 * 4 / pi);
+    a_pdf = @(llr) ones(size(llr, 1), 1) / (4 * pi);
 else
     % Gaussian distribution
-    zenith = zenith_dist(1:2);
     tmp_x = linspace(-90, 90, 50000);
     tmp_pdf = exp(- (90 - tmp_x - zenith(1)).^2/2 / zenith(2)^2) / zenith(2);
-    zen_total = (sum(tmp_pdf) * (tmp_x(2) - tmp_x(1)));
+    zen_total = (sum(tmp_pdf .* cosd(tmp_x)) * (tmp_x(2) - tmp_x(1)) * pi / 180 * 2 * pi);
     a_pdf = @(llr) (exp(- (90 - asind(sind(llr(:, 2))) - zenith(1)).^2/2 / zenith(2)^2) / ...
         zenith(2) / zen_total);
 end
 
-if nargin == 1 || isempty(roll_dist)
-    r_pdf = @(llr) ones(size(llr, 1), 1) / 360;
+% Second part, roll.
+% This PDF represents the density of rolling, i.e. the probability per unit rolling angle.
+% It is normalized so that C * integrate(pdf_roll, d_theta) = 1.
+if nargin == 1 || isempty(roll)
+    r_pdf = @(llr) ones(size(llr, 1), 1) / (2 * pi);
 else
-    roll = roll_dist(1:2);
     tmp_x = linspace(0, 360, 50000);
     tmp_pdf = exp(- (tmp_x - roll(1)).^2/2 / roll(2)^2) / roll(2);
-    zen_total = (sum(tmp_pdf) * (tmp_x(2) - tmp_x(1)));
+    zen_total = (sum(tmp_pdf) * (tmp_x(2) - tmp_x(1)) * pi / 180);
     r_pdf = @(llr) (exp(- (llr(:, 3) - roll(1)).^2/2 / roll(2)^2) / ...
         roll(2) / zen_total);
 end
