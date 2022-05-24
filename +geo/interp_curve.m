ceffed99585b12e06f567560d00b9b714993f476
Jiajie Zhang <zhangjiajie043@gmail.com>
2022-05-24 11:03:01 +0800

    bugfix interp_curve

--- +geo/interp_curve.m
+++ +geo/interp_curve.m
@@ -19,17 +19,17 @@ len_diff = inf;
 while len_diff > 0.1 * ds
     s = unique([(0:ds:s0(end))'; s0], 'sorted');
     pts = interp1(s0, pts0, s, 'spline');
-
+    
     len = sqrt(sum((pts(1:end - 1, :) - pts(2:end, :)).^2, 2));
     tmp_s = cumsum([0; len]);
     tmp_d = pdist2(pts0, pts);
     [~, idx] = min(tmp_d, [], 2);
     len_diff = abs(tmp_s(end) - s(end));
-
+    
     s0 = tmp_s(idx);
     if closed
+        s0(1) = tmp_s(1);
         s0(end) = tmp_s(end);
-        idx(end) = length(tmp_s);
     end
 end
 s = tmp_s;
