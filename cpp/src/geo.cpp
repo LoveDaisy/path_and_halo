5da657568893358a2d8b74ae26fcddd6c71c01c1
Jiajie Zhang <zhangjiajie043@gmail.com>
2022-06-04 03:46:12 +0800

    add geo

--- cpp/src/geo.cpp
+++ cpp/src/geo.cpp
@@ -1,6 +1,58 @@
 #include "geo.hpp"
 
-#include "auto_diff/ad.hpp"
-#include "auto_diff/expr.hpp"
+#include <cassert>
+#include <cmath>
+#include <cstddef>
 
-namespace halo_pm {}  // namespace halo_pm
+namespace halo_pm {
+
+void Ll2Xyz(const float* ll, float* xyz,                    // input & output
+            size_t num,                                     // data number
+            size_t ll_step_bytes, size_t xyz_step_bytes) {  // step of input & output
+  assert(ll_step_bytes % sizeof(float) == 0);
+  assert(xyz_step_bytes % sizeof(float) == 0);
+
+  const float* p = ll;
+  float* q = xyz;
+  for (size_t i = 0; i < num; i++) {
+    q[0] = std::cos(p[1] * kDegree2Rad) * std::cos(p[0] * kDegree2Rad);
+    q[1] = std::cos(p[1] * kDegree2Rad) * std::sin(p[0] * kDegree2Rad);
+    q[2] = std::sin(p[1] * kDegree2Rad);
+
+    p += ll_step_bytes == 0 ? 2 : ll_step_bytes / sizeof(float);
+    q += xyz_step_bytes == 0 ? 3 : xyz_step_bytes / sizeof(float);
+  }
+}
+
+
+void Llr2Mat(const float* llr, float* mat,                    // input & output, llr in degree
+             size_t num,                                      // data number
+             size_t llr_step_bytes, size_t mat_step_bytes) {  // step of input & output
+  assert(llr_step_bytes % sizeof(float) == 0);
+  assert(mat_step_bytes % sizeof(float) == 0);
+
+  const float* p = llr;
+  float* q = mat;
+  for (size_t i = 0; i < num; i++) {
+    auto c1 = std::cos(p[0] * kDegree2Rad);
+    auto s1 = std::sin(p[0] * kDegree2Rad);
+    auto c2 = std::cos(p[1] * kDegree2Rad);
+    auto s2 = std::sin(p[1] * kDegree2Rad);
+    auto c3 = std::cos(p[2] * kDegree2Rad);
+    auto s3 = std::sin(p[2] * kDegree2Rad);
+
+    q[0] = -s1 * c3 - c1 * s2 * s3;
+    q[1] = s1 * s3 - c1 * s2 * c3;
+    q[2] = c1 * c2;
+    q[3] = c1 * c3 - s1 * s2 * s3;
+    q[4] = -c1 * s3 - s1 * s2 * c3;
+    q[5] = s1 * c2;
+    q[6] = c2 * s3;
+    q[7] = c2 * c3;
+    q[8] = s2;
+
+    p += llr_step_bytes == 0 ? 3 : llr_step_bytes / sizeof(float);
+    q += mat_step_bytes == 0 ? 9 : mat_step_bytes / sizeof(float);
+  }
+}
+}  // namespace halo_pm
--- cpp/src/geo.hpp
+++ cpp/src/geo.hpp
@@ -5,12 +5,16 @@
 
 namespace halo_pm {
 
-void Ll2Xyz(const float* ll, float* xyz,                           // input & output
+constexpr float kPi = 3.14159265358f;
+constexpr float kDegree2Rad = kPi / 180.0f;
+constexpr float kRad2Degree = 180.0f / kPi;
+
+void Ll2Xyz(const float* ll, float* xyz,                           // input & output, ll in degree
             size_t num = 1,                                        // data number
             size_t ll_step_bytes = 0, size_t xyz_step_bytes = 0);  // step of input & output
 
 
-void Llr2Mat(const float* llr, float* mat,                           // input & output
+void Llr2Mat(const float* llr, float* mat,                           // input & output, llr in degree
              size_t num = 1,                                         // data number
              size_t llr_step_bytes = 0, size_t mat_step_bytes = 0);  // step of input & output
 
