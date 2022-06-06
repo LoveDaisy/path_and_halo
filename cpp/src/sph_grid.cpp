23342dc105bbcaa218a33d346bb1a1581cd34fc4
Jiajie Zhang <zhangjiajie043@gmail.com>
2022-06-06 16:12:53 +0800

    update

--- cpp/src/sph_grid.cpp
+++ cpp/src/sph_grid.cpp
@@ -11,7 +11,7 @@
 namespace halo_pm {
 
 void FillVec(double z, double phi, float* xyz) {
-  auto sth = std::sqrt(1 - z) * std::sqrt(1 + z);
+  auto sth = std::sqrt((1 - z * z));
   auto x = sth * std::cos(phi);
   auto y = sth * std::sin(phi);
 
@@ -32,11 +32,13 @@ std::tuple<std::unique_ptr<float[]>, size_t> GenerateGrid(int level) {
 
   size_t n_cap = 2 * n_side * (n_side - 1);
   size_t n_eq = 2 * n_side * (5 * n_side + 1);
+  auto fact1 = 1.5 * n_side;
+  auto fact2 = 3.0 * n_side * n_side;
+  size_t nl2 = 2 * n_side;
+  size_t nl4 = 4 * n_side;
 
   // North cap
   for (size_t i = 0; i < n_cap; i++) {
-    auto fact2 = 3.0 * n_side * n_side;
-
     auto hip = (i + 1) / 2.0;
     auto fihip = std::trunc(hip);
     auto i_ring = std::trunc(std::sqrt(hip - std::sqrt(fihip))) + 1;
@@ -50,25 +52,19 @@ std::tuple<std::unique_ptr<float[]>, size_t> GenerateGrid(int level) {
 
   // Equatorial region
   for (size_t i = n_cap; i < n_eq; i++) {
-    size_t nl2 = 2 * n_side;
-    size_t nl4 = 4 * n_side;
-    auto fact1 = 1.5 * n_side;
-
     size_t ip = i - n_cap;
-    auto i_ring = static_cast<size_t>(std::trunc(ip * 1.0 / nl4)) + n_side;
-    auto i_phi = ip % nl4 + 1;
-    auto f_odd = 0.5 * (1 + (i_ring + n_side) % 2);
+    auto i_ring = ip / nl4 + n_side;
+    auto i_phi = ip % nl4;
+    auto f_odd = 0.5 * ((i_ring + n_side) % 2 - 1.0);
 
     auto z = (nl2 * 1.0 - i_ring) / fact1;
-    auto phi = (i_phi - f_odd) * kPi / (2.0 * n_side);
+    auto phi = (i_phi - f_odd) * kPi / nl2;
 
     FillVec(z, phi, data.get() + i * 3);
   }
 
   // South cap
   for (size_t i = n_eq; i < n_pix; i++) {
-    auto fact2 = 3.0 * n_side * n_side;
-
     auto ip = n_pix - i;
     auto hip = ip / 2.0;
     auto fihip = std::trunc(hip);
