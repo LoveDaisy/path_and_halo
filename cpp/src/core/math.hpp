810495778f8998c420b74426aa23218b037bb440
Jiajie Zhang <zhangjiajie043@gmail.com>
2022-06-08 17:26:49 +0800

    add more operator to Vec

--- cpp/src/core/math.hpp
+++ cpp/src/core/math.hpp
@@ -1,11 +1,35 @@
 #ifndef CORE_MATH_H_
 #define CORE_MATH_H_
 
+#include <cmath>
+#include <cstddef>
+
+#include "core/types.hpp"
+
 namespace halo_pm {
 
 constexpr float kPi = 3.14159265358f;
 constexpr float kDegree2Rad = kPi / 180.0f;
 constexpr float kRad2Degree = 180.0f / kPi;
+
+namespace linalgo {
+
+template <class T, size_t N>
+T Norm2(const Vec<T, N>& x) {
+  double n = 0;
+  for (size_t i = 0; i < N; i++) {
+    n += x.data_[i] * x.data_[i];
+  }
+  return n;
+}
+
+
+template <class T, size_t N>
+T Norm(const Vec<T, N>& x) {
+  return std::sqrt(Norm2(x));
+}
+
+}  // namespace linalgo
 
 }  // namespace halo_pm
 
--- cpp/src/core/types.hpp
+++ cpp/src/core/types.hpp
@@ -16,7 +16,10 @@ struct Vec {
 
 template <class T>
 struct Vec<T, 1> {
-  T x_;
+  union {
+    T x_;
+    T data_[1];
+  };
 
   Vec() : x_(0){};
   Vec(T x) : x_(x){};
@@ -25,8 +28,13 @@ struct Vec<T, 1> {
 
 template <class T>
 struct Vec<T, 2> {
-  T x_;
-  T y_;
+  union {
+    struct {
+      T x_;
+      T y_;
+    };
+    T data_[2];
+  };
 
   Vec() : x_(0), y_(0){};
   Vec(T x, T y) : x_(x), y_(y){};
@@ -35,9 +43,14 @@ struct Vec<T, 2> {
 
 template <class T>
 struct Vec<T, 3> {
-  T x_;
-  T y_;
-  T z_;
+  union {
+    struct {
+      T x_;
+      T y_;
+      T z_;
+    };
+    T data_[3];
+  };
 
   Vec() : x_(0), y_(0), z_(0){};
   Vec(T x, T y, T z) : x_(x), y_(y), z_(z){};
@@ -46,10 +59,15 @@ struct Vec<T, 3> {
 
 template <class T>
 struct Vec<T, 4> {
-  T x_;
-  T y_;
-  T z_;
-  T w_;
+  union {
+    struct {
+      T x_;
+      T y_;
+      T z_;
+      T w_;
+    };
+    T data_[4];
+  };
 
   Vec() : x_(0), y_(0), z_(0), w_(0){};
   Vec(T x, T y, T z, T w) : x_(x), y_(y), z_(z), w_(w){};
@@ -75,6 +93,94 @@ struct Mat {
 
 
 using Mat3x3f = Mat<float, 3, 3>;
+
+
+// =============== operators ===============
+// Negative
+template <class T, size_t N>
+Vec<T, N> operator-(const Vec<T, N>& x) {
+  Vec<T, N> res;
+  for (size_t i = 0; i < N; i++) {
+    res.data_[i] = -x.data_[i];
+  }
+  return res;
+}
+
+// Add
+template <class T, size_t N>
+Vec<T, N> operator+(const Vec<T, N>& l, const Vec<T, N>& r) {
+  Vec<T, N> res;
+  for (size_t i = 0; i < N; i++) {
+    res.data_[i] = l.data_[i] + r.data_[i];
+  }
+  return res;
+}
+
+template <class T, size_t N>
+Vec<T, N> operator+(const Vec<T, N>& l, T r) {
+  Vec<T, N> res;
+  for (size_t i = 0; i < N; i++) {
+    res.data_[i] = l.data_[i] + r;
+  }
+  return res;
+}
+
+template <class T, size_t N>
+Vec<T, N> operator+(T l, const Vec<T, N>& r) {
+  Vec<T, N> res;
+  for (size_t i = 0; i < N; i++) {
+    res.data_[i] = l + r.data_[i];
+  }
+  return res;
+}
+
+// Minus
+template <class T, size_t N>
+Vec<T, N> operator-(const Vec<T, N>& l, const Vec<T, N>& r) {
+  Vec<T, N> res;
+  for (size_t i = 0; i < N; i++) {
+    res.data_[i] = l.data_[i] - r.data_[i];
+  }
+  return res;
+}
+
+template <class T, size_t N>
+Vec<T, N> operator-(const Vec<T, N>& l, T r) {
+  Vec<T, N> res;
+  for (size_t i = 0; i < N; i++) {
+    res.data_[i] = l.data_[i] - r;
+  }
+  return res;
+}
+
+template <class T, size_t N>
+Vec<T, N> operator-(T l, const Vec<T, N>& r) {
+  Vec<T, N> res;
+  for (size_t i = 0; i < N; i++) {
+    res.data_[i] = l - r.data_[i];
+  }
+  return res;
+}
+
+// Scalar multiplication
+template <class T, size_t N>
+Vec<T, N> operator*(const Vec<T, N>& l, T r) {
+  Vec<T, N> res;
+  for (size_t i = 0; i < N; i++) {
+    res.data_[i] = l.data_[i] * r;
+  }
+  return res;
+}
+
+template <class T, size_t N>
+Vec<T, N> operator*(T l, const Vec<T, N>& r) {
+  Vec<T, N> res;
+  for (size_t i = 0; i < N; i++) {
+    res.data_[i] = l * r.data_[i];
+  }
+  return res;
+}
+
 
 }  // namespace halo_pm
 
